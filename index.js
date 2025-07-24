0f95c6bbcbdd5e29067509e119b166204491bdd7
Muhamad Alwan Fadhlurrohman <alwan@MacBook-Air-Muhamad.local>
2025-07-24 09:21:07 +0700

    Fix kesalahan enpoint

--- index.js
+++ index.js
@@ -1,40 +1,42 @@
 // server.js
-const express = require('express');
-const cors = require('cors');
-const apiData = require('./data');
-
+const express = require("express");
+const cors = require("cors");
 const app = express();
+
+const apiData = require("./data");
+// console.log("Data dari data.js:", apiData )
 const PORT = process.env.PORT || 3000;
 
 app.use(cors()); // agar bisa diakses dari frontend
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Welcome to Sepatu API');
+app.get("/", (req, res) => {
+  res.send("Welcome to Sepatu API");
 });
 
-app.get('/produk-utama', (req, res) => {
-  res.json(apiData.produk_utama);
+app.get("/produk-utama", (req, res) => {
+  // console.log(apiData.api.produk_utama);
+  res.json(apiData.api.produk_utama);
 });
 
-app.get('/sepatu-lari', (req, res) => {
-  res.json(apiData.sepatu_lari);
+app.get("/sepatu-lari", (req, res) => {
+  res.json(apiData.api.sepatu_lari);
 });
 
-app.get('/sepatu-gunung', (req, res) => {
-  res.json(apiData.sepatu_gunung);
+app.get("/sepatu-gunung", (req, res) => {
+  res.json(apiData.api.sepatu_gunung);
 });
 
 // Detail endpoint by id
-app.get('/sepatu-lari/:id', (req, res) => {
-  const item = apiData.sepatu_lari.find(i => i.id == req.params.id);
-  if (!item) return res.status(404).json({ message: 'Data tidak ditemukan' });
+app.get("/sepatu-lari/:id", (req, res) => {
+  const item = apiData.api.sepatu_lari.find((i) => i.id == req.params.id);
+  if (!item) return res.status(404).json({ message: "Data tidak ditemukan" });
   res.json(item);
 });
 
-app.get('/sepatu-gunung/:id', (req, res) => {
-  const item = apiData.sepatu_gunung.find(i => i.id == req.params.id);
-  if (!item) return res.status(404).json({ message: 'Data tidak ditemukan' });
+app.get("/sepatu-gunung/:id", (req, res) => {
+  const item = apiData.api.sepatu_gunung.find((i) => i.id == req.params.id);
+  if (!item) return res.status(404).json({ message: "Data tidak ditemukan" });
   res.json(item);
 });
 
